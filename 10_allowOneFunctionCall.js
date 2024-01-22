277a16720ca222473806c94a0f86e7ed977f5ae0
Marek Trybuła <trybu001@gmail.com>
2024-01-22 06:53:17 +0100

    allow one function call

---
+++ 10_allowOneFunctionCall.js
@@ -0,0 +1,25 @@
+/*
+    Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+    The first time the returned function is called, it should return the same result as fn.
+    Every subsequent time it is called, it should return undefined.
+*/
+
+const once = function (fn) {
+  let functionCalled = false
+  let result
+
+  return function (...args) {
+    if (functionCalled) return undefined
+    else {
+      result = fn(...args)
+      functionCalled = true
+      return result
+    }
+  }
+}
+
+let fn = (a, b, c) => a + b + c
+let onceFn = once(fn)
+console.log(onceFn(1, 2, 3)) // 6
+console.log(onceFn(2, 3, 6)) // returns undefined without calling fn
