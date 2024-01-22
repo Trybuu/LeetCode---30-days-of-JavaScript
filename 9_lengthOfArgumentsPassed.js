eb7c6e963f7bb6e586a771e0450a237acaa6581c
Marek Trybuła <trybu001@gmail.com>
2024-01-22 06:11:35 +0100

    Length of arguments passed to a function

---
+++ 9_lengthOfArgumentsPassed.js
@@ -0,0 +1,5 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+const argumentsLength = (...args) => args.length
+
+console.log(argumentsLength(1, 2, 3)) // 3
